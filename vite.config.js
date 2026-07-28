13d7af0a99b10d5c10a1cad37c38d7a1919ed5ae
yasir-ghafar <yasir896@gmail.com>
2026-07-28 17:34:20 +0500

    gitignore updated.

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,8 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-<<<<<<< HEAD
     base: '/',    // absolute paths so nested routes still load /assets/* correctly
-=======
-    base: '/',   // works from multiple domains/URLs
->>>>>>> 72fe458fb3f5e849809e17dae0b9e43124442cee
+
   server: {
     host: '0.0.0.0',
     allowedHosts: [
